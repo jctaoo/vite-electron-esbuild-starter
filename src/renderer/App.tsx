2f528950ef4448a7bc24dc273f598e9e7b4de7b7
jctaoo <jctaoo@outlook.com>
2021-02-27 21:22:52 +0800

    update renderer demo

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
-import { add } from '@common/utils';
 import './App.css';
-import { Button } from 'antd';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React</p>
         <p>
-          <Button type="primary" onClick={() => setCount((count) => count + 1)}>
-            count 2sss: {count}
-          </Button>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count {count}
+          </button>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
