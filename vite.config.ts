26590b33f8dbdcefce6a800e19ed331bc22496f3
jctaoo <jctaoo@outlook.com>
2021-02-27 15:14:17 +0800

    move index.html to renderer folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { join, resolve } from "path";
 
-// TODO index.html 不在 root 时有 bug
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   base: "./",
+  root: resolve("./src/renderer"),
+  build: {
+    outDir: resolve("./dist"),
+    emptyOutDir: true
+  },
   resolve: {
     alias: [
       {
